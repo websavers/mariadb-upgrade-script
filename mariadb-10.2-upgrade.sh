9d161c2430713e05782ebce55273b124dfde45ff
Jordan Schelew <jordan@websavers.ca>
2020-02-24 21:16:12 -0600

    Code simplification

--- mariadb-10.2-upgrade.sh
+++ mariadb-10.2-upgrade.sh
@@ -17,61 +17,13 @@ while true; do
 esac
 done
 
-MySQL_VERS_INFO=$(mysql --version)
-
-case $MySQL_VERS_INFO in
-    *"Distrib 5.5."*)
-      UPGRADE_STEPS=3
-      echo "MySQL / MariaDB 5.5 detected."
-      while true; do
-          read -p "Do you wish to attempt to upgrade through to 10.2?" yn
-          case $yn in
-            [Yy]* )
-              CENTOS_MAJOR_VER=$(rpm --eval '%{centos_ver}')
-              echo "# MariaDB 10.0 CentOS repository list - created 2019-02-20 23:18 UTC
-# http://downloads.mariadb.org/mariadb/repositories/
-[mariadb]
-name = MariaDB
-baseurl = http://yum.mariadb.org/10.0/centos$CENTOS_MAJOR_VER-amd64
-gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck=1" > /etc/yum.repos.d/mariadb.repo
-              break
-              ;;
-              
-            [Nn]* ) 
-              exit
-              ;;
-             * ) echo "Please answer yes or no.";;
-           esac
-      done
-      ;;
-      
-    *"Distrib 10.0"*)
-      echo "MariaDB 10.0 detected. Proceeding with full upgrade to 10.2"
-      UPGRADE_STEPS='2'
-      ;;
-      
-    *"Distrib 10.1"*)
-      echo "MariaDB 10.1 detected. Proceeding with partial upgrade to 10.2"
-      UPGRADE_STEPS='1'
-      ;;
-      
-    *"Distrib 10.2"*)
-      echo "Already at 10.2. Exiting."
-      exit 1
-      ;;
-      
-    *)
-      echo "Error. Unknown initial MySQL version. Aborting."
-      exit 1
-      ;;
-esac
-    
-read -p "Are you sure you wish to proceed with the upgrade to 10.2? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
+read -p "Are you sure you wish to proceed with the upgrade to MariaDB 10.2? (y/n)" -n 1 -r
+echo    # new line
 if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
+
+CENTOS_MAJOR_VER=$(rpm --eval '%{centos_ver}')
 
 if [ "$CENTOS_MAJOR_VER" = '7' ]; then
   systemctl stop sw-cp-server
@@ -79,20 +31,13 @@ else
   service sw-cp-server stop
 fi
 
-#install MariaDB 10
-
+#Consistency in repo naming, if one already exists
 if [ -f "/etc/yum.repos.d/MariaDB.repo" ] ; then
   mv /etc/yum.repos.d/MariaDB.repo /etc/yum.repos.d/mariadb.repo
 fi
 
-if [ -f "/etc/yum.repos.d/mariadb.repo" ] ; then
-  echo "MariaDB detected. Proceeding with upgrade..."
-else
-  echo "No MariaDB repo detected at /etc/yum.repos.d/mariadb.repo -- Aborting."
-  exit 1
-fi
 
-do_mariadb_upgrade(){
+function do_mariadb_upgrade{
 
   MDB_VER=$1
   CENTOS_MAJOR_VER=$(rpm --eval '%{centos_ver}')
@@ -106,7 +51,7 @@ baseurl = http://yum.mariadb.org/$MDB_VER/centos$CENTOS_MAJOR_VER-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1" > /etc/yum.repos.d/mariadb.repo
 
-  mv /etc/my.cnf /etc/my.cnf.bak
+  mv -f /etc/my.cnf /etc/my.cnf.bak
 
   yum -y update
   if [ "$CENTOS_MAJOR_VER" = '7' ]; then
@@ -127,25 +72,39 @@ gpgcheck=1" > /etc/yum.repos.d/mariadb.repo
 
 }
 
-case $UPGRADE_STEPS in
-  1)
-    do_mariadb_upgrade '10.2'
-    ;;
-  2)
-    do_mariadb_upgrade '10.1'
-    do_mariadb_upgrade '10.2'
-    ;;
-  3)
-    rpm -e --nodeps mysql-server
-    do_mariadb_upgrade '10.0'
-    do_mariadb_upgrade '10.1'
-    do_mariadb_upgrade '10.2'
-  ;;
-  *)
-    echo Error. Aborting.
-    exit 1
-    ;;
+MySQL_VERS_INFO=$(mysql --version)
+
+case $MySQL_VERS_INFO in
+    *"Distrib 5.5."*)
+      echo "MySQL / MariaDB 5.5 detected. Proceeding with 5.5 -> 10.0 -> 10.1 -> 10.2"
+      rpm -e --nodeps mysql-server
+      do_mariadb_upgrade '10.0'
+      do_mariadb_upgrade '10.1'
+      do_mariadb_upgrade '10.2'
+      ;;
+      
+    *"Distrib 10.0"*)
+      echo "MariaDB 10.0 detected. Proceeding with 10.0 -> 10.1 -> 10.2"
+      do_mariadb_upgrade '10.1'
+      do_mariadb_upgrade '10.2'
+      ;;
+      
+    *"Distrib 10.1"*)
+      echo "MariaDB 10.1 detected. Proceeding with upgrade to 10.2"
+      do_mariadb_upgrade '10.2'
+      ;;
+      
+    *"Distrib 10.2"*)
+      echo "Already at 10.2. Exiting."
+      exit 1
+      ;;
+      
+    *)
+      echo "Error. Unknown initial MySQL version. Aborting."
+      exit 1
+      ;;
 esac
+
 
 ######
 # At completion of all upgrades
@@ -158,6 +117,7 @@ plesk sbin packagemng -sdf
 sed -i 's/^\[mysqld\]/&\nlog-error=\/var\/lib\/mysql\/mysqld.log/' /etc/my.cnf.d/server.cnf
 sed -i 's/^\[mysqld\]/&\nmax_allowed_packet=64M/' /etc/my.cnf.d/server.cnf
 sed -i 's/^\[mysqld\]/&\nopen_files_limit=8192/' /etc/my.cnf.d/server.cnf
+
 if [ "$CENTOS_MAJOR_VER" = '7' ]; then
   systemctl restart mysql
   systemctl restart sw-cp-server
@@ -165,6 +125,7 @@ else
   service mysql restart
   service sw-cp-server restart
 fi
+
 # If the log file hasn't been aliased yet, deal with that
 if [ -f "/var/log/mysqld.log" ]; then
   mv /var/log/mysqld.log /var/log/mysqld.log.bak
